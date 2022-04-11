ba0d584052926693816e9d7f8d912d7f6fe73e77
Devin <devin.ray.broussard@gmail.com>
2022-04-11 16:27:39 -0500

    Trying to fix bouncing bug

--- Assets/Scripts/Movement/BallMovementScript.cs
+++ Assets/Scripts/Movement/BallMovementScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Diagnostics;
 
 public class BallMovementScript : MonoBehaviour
 {
@@ -16,7 +17,19 @@ public class BallMovementScript : MonoBehaviour
     float _ballForce = 250;
 
     /// <summary>
-    /// Awake is called when an instance of the script is created
+    /// The mininum angle that the ball can move towards after a hit
+    /// </summary>
+    [SerializeField]
+    float _wallBounceMultiplier = 1.5f;
+
+    /// <summary>
+    /// The speed that the ball will be reset to after colliding with a paddle
+    /// </summary>
+    [SerializeField]
+    float _defaultBounceSpeed = 100f;
+
+    /// <summary>
+    /// Awake is called when an instace of the script is created
     /// </summary>
     private void Awake()
     {
@@ -24,7 +37,7 @@ public class BallMovementScript : MonoBehaviour
     }
 
     /// <summary>
-    /// Called when the game object is added to the scene
+    /// 
     /// </summary>
     private void Start()
     {
@@ -34,8 +47,20 @@ public class BallMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log($"Velocity: {_rigidbody.velocity.x}, {_rigidbody.velocity.z}");
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+          //  _rigidbody.velocity = _rigidbody.velocity * _wallBounceMultiplier;
+        }
+
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+           // _rigidbody.velocity = (_rigidbody.velocity.normalized * _defaultBounceSpeed);
+        }
+    }
 
 }
---
+++ Assets/Scripts/Movement/PaddleAIBehaviour.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleAIBehaviour : MonoBehaviour
+{
+    /// <summary>
+    /// The ball used in the game
+    /// </summary>
+    [SerializeField]
+    Transform _gameBall;
+
+
+
+    /// <summary>
+    /// Called when this script is added to the scene
+    /// </summary>
+    private void Start()
+    {
+    }
+}
